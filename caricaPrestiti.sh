4ff73f76265801aded30fa3238ff9c3c5af42125
Simone <simone.chimirri@edu-its.it>
2025-06-26 10:22:29 +0200

    Aggiunti file di mongo import per le differenti collezioni

---
+++ caricaPrestiti.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Configurazione
+COLLECTION_NAME="loans"
+FILE_PATH="loans_real.json"
+MONGO_PORT="27017"
+
+# Comando mongoimport
+mongoimport \
+            --uri "mongodb+srv://<username>:<password>@cluster0.ade6fgx.mongodb.net/biblioteca" \
+            --port "$MONGO_PORT" \
+            --collection "$COLLECTION_NAME" \
+            --jsonArray \
+            --file "$FILE_PATH" 
+# Verifica
+if [ $? -eq 0 ]; then
+    echo "Importazione completata con successo."
+else
+    echo "Errore durante l'importazione."
+fi
---
+++ caricaUtenti.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Configurazione
+COLLECTION_NAME="users"
+FILE_PATH="users_real.json"
+MONGO_PORT="27017"
+
+# Comando mongoimport
+mongoimport \
+            --uri "mongodb+srv://<username>:<password>@cluster0.ade6fgx.mongodb.net/biblioteca" \
+            --port "$MONGO_PORT" \
+            --collection "$COLLECTION_NAME" \
+            --jsonArray \
+            --file "$FILE_PATH" 
+# Verifica
+if [ $? -eq 0 ]; then
+    echo "Importazione completata con successo."
+else
+    echo "Errore durante l'importazione."
+fi
