9684e8aebb35c2f52afc290b8a86de236ed689cb
Simone <simone.chimirri@edu-its.it>
2025-06-26 10:12:23 +0200

    Update and rename caricaDati.sh to caricaLibri.sh

--- caricaLibri.sh
+++ caricaLibri.sh
@@ -4,16 +4,15 @@
 DB_NAME="biblioteca"
 COLLECTION_NAME="books"
 FILE_PATH="books_real.json"
-MONGO_HOST="localhost"
 MONGO_PORT="27017"
 
 # Comando mongoimport
-mongoimport --host "$MONGO_HOST" \
+mongoimport \
+            --uri "mongodb+srv://<username>:<password>@cluster0.ade6fgx.mongodb.net/biblioteca" \
             --port "$MONGO_PORT" \
-            --db "$DB_NAME" \
             --collection "$COLLECTION_NAME" \
             --jsonArray \
-            --file "$FILE_PATH"
+            --file "$FILE_PATH" 
 # Verifica
 if [ $? -eq 0 ]; then
     echo "Importazione completata con successo."
